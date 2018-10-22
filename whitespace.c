b765122ba22785d11be85186e9d3e6593e8e2eee
“atishay-js” <atishayjainjsab@gmail.com>
2018-10-23 02:58:31 +0530

    adding space optimization file

---
+++ whitespace.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void main(){
+
+char str[99];
+int i,j,c=0;
+char space=(char)32;									//to print space in the file
+char nextline=(char)10;									//to print nextline
+char backslash=(char)92;
+char n=(char)110;
+
+FILE *fp1,*fp2;
+fp1=fopen("userdata.txt","r");
+fp2=fopen("whitespace.txt","w");
+
+while(fgets(str,99,fp1)!=NULL)								//to extract userdata.c line by line
+{
+	for(i=0;i<strlen(str)-1;i++)
+	{
+		if(strcmp(&str[0])
+		{
+			break;
+		}
+		if(!isspace(str[i]) && !isspace(str[i+1]))	 			 //case when there are continous characters and not any space in between ab,#a,##,b#
+                {
+                        fputc(str[i],fp2);
+                }
+		else if(isalnum(str[i]) && isspace(str[i+1]))				//case when there is an alphabet and then space
+		{
+			fputc(str[i],fp2);
+			for(j=i+2;j<=strlen(str)-1;j++)
+			{
+				if(isalnum(str[j]))					//for case eg int       a
+				{
+					fputc(space,fp2);
+					fputc(str[j],fp2);
+					i=j;
+					break;
+				}
+				else if(!isspace(str[j]) && !isalnum(str[j]))		//for case eg a         +
+				{
+					fputc(str[j],fp2);
+					i=j;
+					break;
+				}
+			}
+		}
+		else if((!isalnum(str[i]) && !isspace(str[i])) && isspace(str[i+1]))
+		{
+			fputc(str[i],fp2);
+			for(j=i+1;j<=strlen(str)-1;j++)
+                        {
+                                if(isalnum(str[j]))                                     //for case eg +       a
+                                {
+                                        fputc(str[j],fp2);
+					i=j;
+                                        break;
+                                }
+                                else if(!isspace(str[j]) && !isalnum(str[j]))           //for case eg +         =
+                                {
+                                        fputc(str[j],fp2);
+					i=j;
+                                        break;
+                                }
+                        }
+		}
+	}
+	fputc(nextline,fp2);
+}
+fclose(fp1);
+fclose(fp2);
+}
