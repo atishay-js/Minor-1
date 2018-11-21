99a1840ca225d69388823b2bc6fa02cba861f987
“atishay-js” <500055748@stu.upes.ac.in>
2018-11-22 01:44:10 +0530

    updated ternary

--- ternary.c
+++ ternary.c
@@ -4,17 +4,22 @@
 void main(){
 
 char str[99];
+char str_nextline[99];
+char str_nextline1[99];
+int str_len;
+char str_nosemi[99];
 char str1[99];
 char str2[99];
 int open=0,close=0;
-int pos_condition,i,j;
+int pos_condition,i,j,coma_pos,pos_neg;
 char condition[10]=" ";
 
-FILE *fp1,*fp2,*fp3,*fp4;
+FILE *fp1,*fp2,*fp3,*fp4,*fp5;
 fp1=fopen("userdata.c","r");
 fp2=fopen("ternary.txt","w+");
-fp3=fopen("tern_content.txt","w+");
+fp3=fopen("tern_if.txt","w+");
 fp4=fopen("if_complete.txt","w+");
+fp5=fopen("tern_else.txt","w+");
 
 while(fgets(str,99,fp1)!=NULL)
 {
@@ -32,6 +37,17 @@ while(fgets(str,99,fp1)!=NULL)
 		do
 		{
 			fputs(str,fp4);
+			if(strstr(str,";")!=NULL)
+                	{
+		        	str_len=strlen(str);
+				memset(str_nosemi,0,str_len);
+				j=0;
+		                for(i=0;i<str_len-2;i++)
+		                {
+		                	str_nosemi[j]=str[i];
+		                        j++;
+		                }
+			}
 			if(strstr(str,"{")!=NULL)                                                       	//checking for "{" in the same line
                 	{
                         	if(open==0)
@@ -39,7 +55,8 @@ while(fgets(str,99,fp1)!=NULL)
 				else
 				{
 					open++;
-					fputs(str,fp3);
+					fputs(str_nosemi,fp3);
+					fputs("\n",fp3);
 				}
 			}
                 	else if(strstr(str,"}")!=NULL)                                                       	//checking for "}"
@@ -49,16 +66,19 @@ while(fgets(str,99,fp1)!=NULL)
                         	{
 					if(strstr(str,"else")!=NULL)
                 			        goto b;								//else found in same line as }else
-                                	break;                                                         	//therefore goto a i.e. end of for and out of for
+                                	break;                                                         		//therefore goto a i.e. end of for and out of for
                         	}
-				fputs(str,fp3);
+				fputs(str_nosemi,fp3);
+				fputs("\n",fp3);
                 	}
 			else
 			{
-				fputs(str,fp3);
+				fputs(str_nosemi,fp3);
+				fputs("\n",fp3);
 			}
 			fgets(str,99,fp1);
 		}while((open-close)!=0);
+
 		fgets(str,99,fp1);
 		if(strstr(str,"else")!=NULL)
 		{
@@ -72,8 +92,40 @@ while(fgets(str,99,fp1)!=NULL)
 		open=0;
 		close=0;
 		fputs("~\n",fp3);
+		fseek(fp3,0,SEEK_SET);
+                fgets(str_nextline,99,fp3);
+		j=0;
+                while(strstr(str_nextline1,"~")==NULL)
+                {
+                        fgets(str_nextline1,99,fp3);
+			if(strstr(str_nextline1,"~")==NULL)
+                        {
+                                j=j+strlen(str_nextline);
+                               	memset(str_nextline,0,99);
+                                fseek(fp3,j-1,SEEK_SET);
+                                fputs(",",fp3);
+                                fseek(fp3,j+strlen(str_nextline1),SEEK_SET);
+                                strcpy(str_nextline,str_nextline1);
+			}
+                        else
+                                break;
+                }
+		fseek(fp3,0,SEEK_END);
 		do
                 {
+			if(strstr(str,";")!=NULL)
+                	{
+		        	str_len=strlen(str);
+				memset(str_nosemi,0,str_len);
+				j=0;
+		                for(i=0;i<str_len-2;i++)
+		                {
+		                	str_nosemi[j]=str[i];
+		                        j++;
+		                }
+				//str_nosemi[j]=',';
+				//puts(str_nosemi);
+              		}
                         if(strstr(str,"{")!=NULL)                                                               //checking for "{"
                         {
                                 if(open==0)
@@ -81,7 +133,8 @@ while(fgets(str,99,fp1)!=NULL)
                                 else
                                 {
                                         open++;
-                                        fputs(str,fp3);
+                                        fputs(str_nosemi,fp5);
+					fputs("\n",fp5);
                                 }
                         }
                         else if(strstr(str,"}")!=NULL)                                                               //checking for "}"
@@ -91,16 +144,42 @@ while(fgets(str,99,fp1)!=NULL)
                                 {
                                        break;                                                          		//therefore goto a i.e. end of for and out of for
                                 }
-                                fputs(str,fp3);
+                                fputs(str_nosemi,fp5);
+				fputs("\n",fp5);
                         }
                         else
                         {
-                                fputs(str,fp3);
-                        }
+                                fputs(str_nosemi,fp5);
+				fputs("\n",fp5);
+			}
                         fgets(str,99,fp1);
+			//if(strstr(str,"}")==NULL || (open-(close+1))!=0)
+			//	fputs(",",fp3);
                 }while((open-close)!=0);
+		fputs("~\n",fp5);
+		fseek(fp5,0,SEEK_SET);
+		memset(str_nextline1,0,99);
+                fgets(str_nextline,99,fp5);
+                j=0;
+                while(strstr(str_nextline1,"~")==NULL)
+                {
+                        fgets(str_nextline1,99,fp5);
+                        if(strstr(str_nextline1,"~")==NULL)
+                        {
+                                j=j+strlen(str_nextline);
+                                memset(str_nextline,0,99);
+                                fseek(fp5,j-1,SEEK_SET);
+                                fputs(",",fp5);
+                                fseek(fp5,j+strlen(str_nextline1),SEEK_SET);
+                                strcpy(str_nextline,str_nextline1);
+                        }
+                        else
+                                break;
+                }
+		fseek(fp5,0,SEEK_END);
+
 		fputs(condition,fp2);
-		fputs("?",fp2);
+		fputs("?(",fp2);
 		fseek(fp3,0,SEEK_SET);
 		while(fgets(str2,99,fp3)!=NULL)
 		{
@@ -108,12 +187,15 @@ while(fgets(str,99,fp1)!=NULL)
 				break;
 			fputs(str2,fp2);
 		}
-		fputs(":\n",fp2);
-		while(fgets(str2,99,fp3)!=NULL)
+		fputs("):\n(",fp2);
+		fseek(fp5,0,SEEK_SET);
+		while(fgets(str2,99,fp5)!=NULL)
 		{
+			if(strstr(str2,"~")!=NULL)
+				break;
 			fputs(str2,fp2);
 		}
-		fputs(";\n",fp2);
+		fputs(");\n",fp2);
 		c:
 		while(fgets(str1,99,fp4)!=NULL)
 		{
@@ -131,25 +213,3 @@ fclose(fp2);
 fclose(fp3);
 fclose(fp4);
 }
-
-
-
-
-
-
-
-/*
-do{
-	condition=fputs(str,fp2);
-	}
-	while(strstr(str,")")!=NULL);
-
-	for(strstr(str,"{")!=NULL;strstr(str,"}")!=NULL;str++)
-	{
-	state1=fputs(str,fp2);
-	}
-
-	}
-
-	if(strstr(str,"else")
-*/
