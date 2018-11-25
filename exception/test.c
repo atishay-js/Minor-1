ada0c81be014f09eb0cb2b8b042fc0923b62b302
verma26shubhi <43296683+verma26shubhi@users.noreply.github.com>
2018-11-25 23:23:46 +0530

    Update test.c

--- exception/test.c
+++ exception/test.c
@@ -2,14 +2,14 @@
 #include "exception.h"
 
 int main(int argc, char **argv) {
-	exception_init();
+	initialize_exception();                     //calling the initialize exception method
 	try
 		printf("Ok!\n");
-		throw("An error happened", 1);
+		throw("An error happened", 1);      //throw must contain error message and error, itself
 		printf("Super Ok!\n");
 	catch(err)
-		fprintf(stderr, "Error %d: %s\n", err.code, err.msg);
-	end_try_catch
-	exception_end();
+		fprintf(stderr, "Error %d: %s\n", err.code, err.msg);       //note:fprintf is used
+	end_try_catch 
+	exception_end();                            //calling the end exception method
 	return 0;
 }
