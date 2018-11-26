131231ed6f39ceec84f594bbe5fa0857f15d66a3
verma26shubhi <43296683+verma26shubhi@users.noreply.github.com>
2018-11-26 18:54:59 +0530

    Update test.c

--- exception/test.c
+++ exception/test.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
 		throw("An error happened", 1);      //throw must contain error message and error, itself
 		printf("Super Ok!\n");
 	catch(err)
-		fprintf(stderr, "Error %d: %s\n", err.code, err.msg);       //note:fprintf is used
+		fprintf(stderr, "Error %d: %s\n", err.code, err.msg);       //note:fprintf is used for displaying error using stderr
 	end_try_catch 
 	exception_end();                            //calling the end exception method
 	return 0;
