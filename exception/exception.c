31a09ef772816866438c300c89304fa21f0de824
verma26shubhi <43296683+verma26shubhi@users.noreply.github.com>
2018-11-26 18:54:11 +0530

    Update exception.c

--- exception/exception.c
+++ exception/exception.c
@@ -5,22 +5,22 @@
 #include <string.h>
 
 
-jmp_buf *bufers;
-size_t bufer_count = 0;
+jmp_buf *bufers;                                               //array is declared of type jmp_buf
+size_t bufer_count = 0;                                        //variable count is initialized of type size_t 
 
-exception_type last_exception;
+exception_type last_exception;                                 //exception_type is a enum datatype which contain a integer code and a char msg, last_exception is its variable 
 
 
 void initialize_exception() {
-	bufers = (jmp_buf*) malloc(0);
-	last_exception.code = 0;
+	bufers = (jmp_buf*) malloc(0);                         //bufer=NULL,it helps to free memory
+	last_exception.code = 0;                              
 	last_exception.msg = (char*) malloc(0);
 }
 
 jmp_buf *exception_newbuf() {
 	bufer_count++;
-	bufers = (jmp_buf*) realloc((void*) bufers, sizeof(jmp_buf) * bufer_count);
-	return &bufers[bufer_count-1];
+	bufers = (jmp_buf*) realloc((void*) bufers, sizeof(jmp_buf) * bufer_count);              //resize the memory block of bufer
+	return &bufers[bufer_count-1];                           //return bufer array
 }
 
 void exception_endbuf() {
@@ -31,14 +31,14 @@ void exception_endbuf() {
 void throw(const char *msg, int code) {
 	last_exception.code = code;
 	if (!bufer_count) {
-		fprintf(stderr, "Uncaught exception of code %d and message: %s\n", code, msg);
+		fprintf(stderr, "Uncaught exception of code %d and message: %s\n", code, msg);       //printing exception message using stderr
 		exception_end();
-		raise(SIGSEGV);
+		raise(SIGSEGV);                                 //it is raising a signal
 	}
-	free((void*) last_exception.msg);
-	last_exception.msg = (char*) malloc( sizeof(char)*(strlen(msg)+1) );
-	strcpy(last_exception.msg, msg);
-	longjmp(bufers[bufer_count-1], code);
+	free((void*) last_exception.msg);                      //freeing the message memory
+	last_exception.msg = (char*) malloc( sizeof(char)*(strlen(msg)+1) );        //allocating the memory to msg
+	strcpy(last_exception.msg, msg);                       
+	longjmp(bufers[bufer_count-1], code);                   //calling longjmp 
 }
 
 const exception_type exception_get_last(){
@@ -48,6 +48,6 @@ const exception_type exception_get_last(){
 void exception_end() {
 	bufer_count = 0;
 	last_exception.code = 0;
-	free((void*) last_exception.msg);
-	free((void*) bufers);
+	free((void*) last_exception.msg);                      //freeing memory of msg
+	free((void*) bufers);                                  //freeing memory of array bufers
 }
